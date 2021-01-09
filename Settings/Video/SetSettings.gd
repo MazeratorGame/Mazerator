8c8ab44a241e26963c332c360c02e2dccad41d33
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2021-01-09 18:24:08 +0000

    Continued video settings
    May need help on this.

--- Settings/Video/SetSettings.gd
+++ Settings/Video/SetSettings.gd
@@ -28,8 +28,8 @@ func _init():
 		ProjectSettings.set_setting("rendering/quality/voxel_cone_tracing/high_quality", hd)
 		ProjectSettings.set_setting("rendering/quality/depth/hdr", hdr)
 		ProjectSettings.set_setting("rendering/quality/dynaimc_fonts/use_oversampling", fo)
-		ProjectSettings.set_setting("display/window/size/width", screen_width)
-		ProjectSettings.set_setting("display/window/size/height", screen_height)
+		ProjectSettings.set_setting("display/window/size/test_width", screen_width)
+		ProjectSettings.set_setting("display/window/size/test_height", screen_height)
 		ProjectSettings.set_setting("display/window/size/fullscreen", fullscreen)
 			
 	else:
@@ -88,6 +88,13 @@ func _on_Video_Settings_ready():
 	
 	$TextEdit.text = str(dss)
 	$TextEdit2.text = str(sas)
+	
+	if ssq == 1:
+		$Button3.text = "Subsurface Scattering quality: Medium"
+	elif ssq == 2:
+		$Button3.text = "Subsurface Scattering quality: High"
+	elif ssq == 0:
+			$Button3.text = "Subsurface Scattering quality: Low"
 
 
 func _on_TextEdit_text_changed():
@@ -100,3 +107,81 @@ func _on_TextEdit2_text_changed():
 	sas = int($TextEdit2.text)
 	config.set_value("sas", "size", 4096)
 	ProjectSettings.set_setting("rendering/quality/shadow_atlas/size", sas)
+
+
+func _on_Button3_pressed():
+	if ssq == 1:
+		ssq = 2
+		$Button3.text = "Subsurface Scattering quality: High"
+	elif ssq == 2:
+		ssq = 0
+		$Button3.text = "Subsurface Scattering quality: Low"
+	elif ssq == 0:
+		ssq = 1
+		$Button3.text = "Subsurface Scattering quality: Medium"
+		
+	config.set_value("ssq", "quality", ssq)
+	ProjectSettings.set_setting("rendering/quality/subsurface_scattering/quality", ssq)
+	config.save("user://video.cfg")
+
+
+func _on_Button4_pressed():
+	if fs == false:
+			fs = true
+			$Button4.text = "Follow Surface: On"
+	else:
+		fs = false
+		$Button4.text = "Follow Surface: Off"
+			
+	config.set_value("fs", "value", fs)
+	ProjectSettings.set_setting("rendering/quality/subsurface_scattering/follow_surface", fs)
+	config.save("user://video.cfg")
+
+
+func _on_Button5_pressed():
+	if hd == false:
+		hd = true
+		$Button5.text = "HD Reflections: On"
+	else:
+		hd = false
+		$Button5.text = "HD Reflections: Off"
+			
+	config.set_value("hd", "reflections", hd)
+	ProjectSettings.set_setting("rendering/quality/voxel_cone_tracing/high_quality", hd)
+	config.save("user://video.cfg")
+
+
+func _on_Button6_pressed():
+	if hdr == false:
+		hdr = true
+		$Button6.text = "HDR: On"
+	else:
+		hdr = false
+		$Button6.text = "HDR: Off"
+			
+	config.set_value("hd", "hdr", hdr)
+	ProjectSettings.set_setting("rendering/quality/depth/hdr", hdr)
+	config.save("user://video.cfg")
+
+
+func _on_Button7_pressed():
+	if fo == false:
+		fo = true
+		$Button7.text = "Font Oversampling: On"
+	else:
+		fo = false
+		$Button7.text = "Font Oversampling: Off"
+			
+	config.set_value("fo", "value", fo)
+	ProjectSettings.set_setting("rendering/quality/dynaimc_fonts/use_oversampling", fo)
+	config.save("user://video.cfg")
+
+
+func _on_Button8_pressed():
+	screen_width = int($TextEdit3.text)
+	screen_height = int($TextEdit4.text)
+	ProjectSettings.set_setting("display/window/size/width", screen_width)
+	ProjectSettings.set_setting("display/window/size/height", screen_height)
+	config.set_value("screen_size", "width", screen_width)
+	config.set_value("screen_size", "height", screen_height)
+	config.save("user://video.cfg")
