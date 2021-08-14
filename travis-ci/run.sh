7fb7ae4aca66a4482cb43879e89877172661ad91
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2021-08-14 19:10:05 +0100

    Update run.sh

--- travis-ci/run.sh
+++ travis-ci/run.sh
@@ -1,13 +1,13 @@
 sudo apt-get update
 sudo apt-get -y install wget zip unzip bzip2
-wget -q https://downloads.tuxfamily.org/godotengine/3.2.2/Godot_v3.2.2-stable_linux_headless.64.zip
-unzip Godot_v3.2.2-stable_linux_headless.64.zip
-mv Godot_v3.2.2-stable_linux_headless.64 godot
+wget -q https://downloads.tuxfamily.org/godotengine/3.3/Godot_v3.3-stable_linux_headless.64.zip
+unzip Godot_v3.3-stable_linux_headless.64.zip
+mv Godot_v3.3-stable_linux_headless.64 godot
 chmod +x godot
-wget -q https://downloads.tuxfamily.org/godotengine/3.2.2/Godot_v3.2.2-stable_export_templates.tpz
+wget -q https://downloads.tuxfamily.org/godotengine/3.3/Godot_v3.3-stable_export_templates.tpz
 mkdir -p ~/.local/share/godot/templates
-unzip Godot_v3.2.2-stable_export_templates.tpz -d ~/.local/share/godot/templates
-mv ~/.local/share/godot/templates/templates ~/.local/share/godot/templates/3.2.2.stable
+unzip Godot_v3.3-stable_export_templates.tpz -d ~/.local/share/godot/templates
+mv ~/.local/share/godot/templates/templates ~/.local/share/godot/templates/3.3.stable
 mkdir win64
 ./godot --path project.godot --export win64 "win64/Mazerator.exe"
 zip win64.zip win64/*
